3eab2f77b648c78a78a2b78cddb4eac38a43c67c
wangheng <wangheng@vchangyi.com>
2021-11-20 22:50:01 +0800

    eslit&prettier-fix

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,19 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String
-})
-
-const count = ref(0)
+<script>
+import { ref } from 'vue';
+export default {
+  props: {
+    msg: {
+      type: String,
+      default: ''
+    }
+  },
+  setup() {
+    const count = ref(0);
+    return {
+      count
+    };
+  }
+};
 </script>
 
 <template>
@@ -29,7 +37,8 @@ const count = ref(0)
   <button type="button" @click="count++">count is: {{ count }}</button>
   <p>
     Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
+    <code>components/HelloWorld.vue</code>
+    to test hot module replacement.
   </p>
 </template>
 
