1b0f9c8953d127dfbdac6f8744679330b83c2986
wangheng <630352167@qq.com>
2021-11-21 20:09:30 +0800

    更新全局样式

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,15 @@ import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <img class="m-t-50 m-b-50" alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
 <style lang="less">
 #app {
   margin-top: 60px;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   text-align: center;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   color: #2c3e50;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
+// @ts-nocheck
 import { createApp } from 'vue';
 import App from './App.vue';
+import './styles/index.less';
 
 createApp(App).mount('#app');
