01ef3349b04635ed8d82c14a4b1af0bf0dedbe07
wangheng <630352167@qq.com>
2021-11-23 21:42:42 +0800

    chore: 测试commitlint

--- .cz-config.js
+++ .cz-config.js
@@ -6,7 +6,7 @@ module.exports = {
     { value: '🐛fix', name: 'fix: 修复bug' },
     { value: '⚡️perf', name: 'perf: 性能优化' },
     { value: '♻️refactor', name: 'refactor: 代码重构' },
-    { value: '🛠️chore', name: 'chore: 更改配置文件' },
+    { value: 'chore', name: '🛠️ chore: 更改配置文件' },
     { value: '➕add', name: 'add: 添加依赖' },
     { value: '❌del', name: 'del: 删除代码/文件' },
     { value: '🎨style', name: 'style: 样式修改不影响逻辑' },
@@ -16,19 +16,11 @@ module.exports = {
     { value: '⌨️release', name: 'release: 发布' },
     { value: '👷deploy', name: 'deploy: 部署' }
   ],
-  scopes: [],
   messages: {
     type: '选择更改类型:\n',
-    // scope: '更改的范围:\n',
-    // 如果allowcustomscopes为true，则使用
-    // customScope: 'Denote the SCOPE of this change:',
-    customScope: '更改的范围:\n',
     subject: '简短描述:\n',
     body: '详细描述. 使用"|"换行:\n',
-    breaking: 'Breaking Changes列表:\n',
-    footer: '关闭的issues列表. E.g.: #31, #34:\n',
     confirmCommit: '确认提交?'
   },
-  allowCustomScopes: true,
-  allowBreakingChanges: ['feat', 'fix']
+  skipQuestions: ['scope', 'footer']
 };
